edaa657bbf425b0597877b216b3907a98a5db5df
motorpsiq <70299980+motorpsiq@users.noreply.github.com>
2021-09-09 18:01:19 -0300

    Clases 8, 9 y 10
    array

---
+++ 1practica_funciones.py
@@ -0,0 +1,43 @@
+# print("Hola " + "Mundo")
+#Asignación de valor a las variables
+# a=45
+# b=45
+# print(a)
+# print(b)
+# suma=a + b
+# print(suma)
+
+#obtener el largo de la cantidad de caracteres
+# caracter='Vacaciones'
+# print(len(caracter))
+
+# ################################# cadena vacia
+# m=""
+# print(m)
+
+################################# #recorrer una cadena de caracteres
+
+# s="Salud"
+# for x in s:
+#     print(x)
+
+################################# # Acceder a la posición de un caracter determinado
+# cadena="Espacio"
+# print("El primer caracter es: ", cadena[0])
+
+# print("El caracter buscado es: ", cadena[-7])
+
+# # ####################Aplicar un metodo para convertir las cadenas o strings en mayusculas o minisculas
+# cadena="Rinoceronte"
+# print(cadena.upper())# a MAYUSCULAS
+# print(cadena.lower())# a minusculas
+
+# ######################################Conocer la posición de un caracter
+# cadena="elefante"
+# print(cadena.find("xte"))#si el resultado es -1, significa que NO fue encontrado
+# print(cadena.find("fan"))
+
+# print(cadena.count("e"))#count esta función cuenta las apariciones del caracter solicitado
+
+###################################Usar un intérvalo de búsqueda que lo busque desde la posicion 3 hasta la 8
+# print(cadena.find("e",3,8))
---
+++ 2practica_array.py
@@ -0,0 +1,38 @@
+#Listas
+#/////////////////////7como defino listas
+lista = [1,2,3,"marte","mercurio"]
+print(lista)
+#print(lista[0])
+#for elemento in lista:
+#   print(elemento)
+#Listas y sus metodos
+#-------------------
+#para agregar append
+#lista.append("saturno")
+#print(lista)
+#agregar una lista en otra
+#lista.append([1,"luna"])
+#print(lista)
+#print(lista[5])
+#----cantidad de veces que aparece un elemento
+#lista.append(1)
+#print(lista)
+#print(lista.count(1))
+#----Para eleminar, usar el metodo remove
+#lista.remove(1)
+#print (lista)
+#----Para conocer la posición de un elemento
+#print(lista.index(1))
+######CONJUNTOS
+#c1={1,2,3,4,5}
+#c2={2,4,6,8,10}
+#union
+#print(c1|c2)
+#intersección & (elementos comunes en ambos conjuntos
+#print(c1&c2)
+#diferencia - (elementos que estan en c1 y no en c2)
+#print(c1-c2)
+##########DICCIONARIOS--permiten almacenar conjunto de valores de la forma clave:valor
+# cel = {"miguel":1,"jose":2,"lucas":8}
+# for clave,valor in cel.items():
+#     print(clave,valor)
